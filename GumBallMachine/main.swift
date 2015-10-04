2841c8ad5c0d90d323a6e5c9b3a42dc473e24b1a
takushi <takushi@mfmf.me>
2015-10-04 16:12:19 +0900

    Initial Commit

---
+++ GumBallMachine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GumBallMachine
+//
+//  Created by Homma Takushi on 2015/10/04.
+//  Copyright © 2015年 mfmf.me. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
